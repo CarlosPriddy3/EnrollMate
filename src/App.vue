b89c57bb2cda479f9b3af862301b0b071594f486
David Le <davidhle721@gmail.com>
2018-09-07 01:51:46 -0400

    Nav bar & basic routing

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Nav></Nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Nav'
+  export default {
+    name: 'app',
+    components: {
+      'Nav': Nav
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #2c3e50;
+    margin-top: 0px;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Here is some information. This is the home page</p>
+  </div>
+</template>
+
+<script>
+	export default {
+  	name: 'Home' //this is the name of the component
+	}
+</script>
+
+<style>
+	h1, p {
+		margin: 30px;
+	}
+</style>
