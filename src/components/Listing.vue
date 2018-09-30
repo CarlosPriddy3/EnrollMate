4dc5aba7c807dc36b368de60516f706ec32fa73f
Zachary Egger <zacegger@gmail.com>
2018-09-30 15:45:02 -0400

    cleanup to run

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,23 +1,12 @@
 <template>
 	<div class="root">
-		/*<div class="search">
-	 		<v-text-field v-model="search" solo label="Search Subject..." append-icon="search"></v-text-field>
-  	</div>*/
 		<div class="listing" style="overflow-y: scroll;">
 	    <v-expansion-panel expand>
 	      <v-expansion-panel-content v-for="(subject, index) in subjects" :key="subject.id">
 	      	<div slot="header">{{ subject.id }}</div>
-	      	<v-expansion-panel-content class="grey lighten-1" v-for="course in subArrs[index]" :key="course.id">
+	      	<v-expansion-panel-content class="grey lighten-3" v-for="course in accRef" :key="course.id">
 	      		<div slot="header">{{ course.SUBJ + " Instructor: " + course.FIRST_NAME
 	      		                      + " " + course.INSTRUCTOR + " " + course.CURRENT_ENRL + "/" + course.MAX_ENRL }}</div>
-	      		<v-card>
-	          	<v-card-text class="grey lighten-3">90702  210 01     ACCOUNTING FOR BUSINESS           4.0   53   53        0    0 MW      01:00PM 02:00PM BAB   121        Bryson Roscoe    </v-card-text>
-     					<v-card-text class="grey lighten-3">90702  210 01     ACCOUNTING FOR BUSINESS           4.0   53   53        0    0 MW      11:20AM 12:40PM BAB   121        Bryson Roscoe    </v-card-text>
-     					<v-card-text class="grey lighten-3">90703  210 02     ACCOUNTING FOR BUSINESS           4.0   53   53        0    0 TR      11:20AM 12:40PM BAB   123        Zhang Jing     </v-card-text>
-     					<v-card-text class="grey lighten-3">90703  210 02     ACCOUNTING FOR BUSINESS           4.0   53   53        0    0 TR      01:00PM 02:00PM BAB   123        Zhang Jing    </v-card-text>
-     					<v-card-text class="grey lighten-3">90704  210 03     ACCOUNTING FOR BUSINESS           4.0   53   50        3    0 MW      11:20AM 12:20PM BAB   123        Hoskins John   </v-card-text>
-     					<v-card-text class="grey lighten-3">90704  210 03     ACCOUNTING FOR BUSINESS           4.0   53   50        3    0 MW      09:40AM 11:00AM BAB   123        Hoskins John</v-card-text>
-	        	</v-card>
 	      	</v-expansion-panel-content>
 	      </v-expansion-panel-content>
 	    </v-expansion-panel>
@@ -60,15 +49,15 @@
       }
     },
     firebase: {
-          accRef: db.ref('/').orderByChild('SUBJECT').equalTo('ACC'),
-          maRef: db.ref('/').orderByChild('SUBJECT').equalTo('MA')
-        },
+          accRef: db.ref('/').orderByChild('SUBJ').equalTo('ACC'),
+          maRef: db.ref('/').orderByChild('SUBJ').equalTo('MA')
+        }/*,
     computed: {
           subArrs: [
           {accRef},
           {maRef}
           ]
-    }
+    }*/
   }
 </script>
 
