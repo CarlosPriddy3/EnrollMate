cbc078bc24958fbf1c8b08b09c0ab8f2c02dcbc8
csmith424 <csmith424@gatech.edu>
2018-11-11 21:04:19 -0500

    filtering added to listing (broken rn)
    filtering added to listing but not yet working

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-  	<!-- <Search></Search> -->
-    <Filtering></Filtering>
     <Listing></Listing>
   </div>
 </template>
 
 <script>
-  import Filtering from './Filtering'
 	import Listing from './Listing'
 	export default {
   		name: 'Home',
   		components: {
   			'Listing': Listing,
-        'Filtering': Filtering
   		}
 	}
 </script>
--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -1,8 +1,59 @@
 <template>
-	<div class="root">
+	<div class="root" id="course_listing">
     <!-- <div class="search">
       <v-text-field v-model="search" solo label="Search College..." append-icon="search"></v-text-field>
     </div> -->
+
+    <div class="filter_panel" id="filtering">
+      <div class="dropdown">
+        <select v-model="college" id="colleges">
+          <option disabled value="">Colleges</option>
+          <option>Arts, Humanities, and Social Sciences</option>
+          <option>Business</option>
+          <option>Education</option>
+          <option>Engineering</option>
+          <option>Nursing</option>
+          <option>Professional and Continuing Studies</option>
+          <option>Science</option>
+          <option>Graduate School</option>
+          <option>Honors</option>
+        </select>
+        <br>
+        <span class="results"><b>Selected College: {{ college }}</b></span>
+      </div>
+      <div class="dropdown">
+        <select v-model="credit_hours" id="credit_hours">
+          <option disabled value="">Credit Hours</option>
+          <option>1 Credit Hour</option>
+          <option>2 Credit Hours</option>
+          <option>3 Credit Hours</option>
+          <option>4 Credit Hours</option>
+        </select>
+        <br>
+        <span class="results"><b>Selected Credit Hours: {{ credit_hours }}</b></span>
+      </div>
+      <div class="checkboxes" id="days">
+        <input type="checkbox" id="monday" value="M" v-model="checkedDays">
+        <label for="monday">M </label>
+        <input type="checkbox" id="tuesday" value="T" v-model="checkedDays">
+        <label for="tuesday">T </label>
+        <input type="checkbox" id="wednesday" value="W" v-model="checkedDays">
+        <label for="wednesday">W </label>
+        <input type="checkbox" id="thursday" value="R" v-model="checkedDays">
+        <label for="thursday">R </label>
+        <input type="checkbox" id="friday" value="F" v-model="checkedDays">
+        <label for="friday">F </label>
+        <br>
+        <span class="results"><b>Checked days: {{ checkedDays }}</b></span>
+      </div>
+      <div class="search_bar" id="search_course">
+        <input v-model="searchResult" placeholder="Search for a course...">
+      </div>
+      <div class="submit" id="submit_button">
+        <button class="submit_botton" v-on:click="filterList()">See Results</button>
+      </div>
+            
+    </div>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
 		<div class="listing" style="overflow-y: scroll;">
@@ -63,6 +114,7 @@
   let db = firebaseApp.database()
   import HashSet from 'hashset'
   export default {
+    name: 'course_listing',
     firebase: {
               dbRef: db.ref('/')
     },
@@ -70,6 +122,11 @@
       return {
         search: '',
         instructor: '',
+        college: '',
+        credit_hours: '',
+        checkedDays: [],
+        days: '',
+        searchResult: '',
         selectedFilters: [],
         subjects: [
         {id: 'ACC', classname: 'Accounting'},
@@ -108,12 +165,6 @@
       }
     },
 
-    mounted() {
-      EventBus.$on('filtersSelected', allFilters => {
-        this.selectedFilters = allFilters;
-      });
-    },
-
     computed: {
         // List of classes - Default 0 filters
         filteredList: function() {
@@ -128,16 +179,30 @@
               profSet.add("" + dbRef[key].INSTRUCTOR + ", " + dbRef[key].FIRST_NAME);
             }
             return profSet.toArray().sort();
+        },
+        listOfFilters: function() {
+          var i;
+          var j;
+          for (i = 0; i < this.checkedDays.length; i++) {
+            this.days += this.checkedDays[i];
+          }
+          this.selectedFilters.push(this.college);
+          this.selectedFilters.push(this.credit_hours);
+          this.selectedFilters.push(this.days);
+          this.selectedFilters.push(this.searchResult);
+          return selectedFilters;
         }
     },
     methods: {
+        
         //Filters the filteredList by given searchterms in the following format:
       	//ObjectLiteral format: {DAYS: {filter0: "MWF", filter1: "TR"}, INSTRUCTOR: {filter0: "Waddell, Emmanuel"} }
-      	filterBySearch: function(searchTerms) {
+      	filterBySearch: function() {
       	  //For each search term, filter our list
       	  filteredList = this.dbRef.slice()
-      	  for (var key in Object.keys(searchTerms)) {
-      	      filteredList = filterList(filteredList.slice(), key, searchTerms[key]) //key should be 'MWF'
+          var ind; 
+      	  for (ind = 0; i < selectedFilters.length; ind++) {
+      	      filteredList = filterList(filteredList.slice(), selectedFilters); //key should be 'MWF'
       	  }
       	},
       	//Takes any list and any HEADER in the CSV file as a filterType and filters by value
@@ -156,6 +221,73 @@
 </script>
 
 <style>
+  .filter_panel{
+    margin:75px;
+    margin-bottom: 25px;
+    text-align: left;
+    max-width: 95%;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    font-size: 14px;
+    padding: 16px 8px 8px 16px;
+    background: #fff;
+    overflow: hidden;
+    position: relative;
+  }
+
+  .filter_panel .dropdown{
+    width: 100%;
+    background-position: 80% relative;
+    padding: 8px 8px 8px 8px;
+    font-size:15px;
+    margin-bottom: 16px;
+  }
+
+  .filter_panel .dropdown select{
+    background: transparent;
+    line-height: 1;
+    border: 0;
+    padding: 0;
+    border-radius: 0;
+    width: 48%;
+    position: relative;
+    z-index: 10;
+    font-size: 1em;
+  }
+
+  .filter_panel .checkboxes{    
+    padding: 8px 8px 8px 8px;
+    font-size: 16px;
+    margin-bottom: 16px;
+  }
+
+  .filter_panel .results{
+    font-size: 14px;
+    margin-bottom: 6px;
+  }
+
+  .filter_panel .p{
+    font-size: 14px;
+    margin-bottom: 6px;
+  }
+
+  .search_bar {
+    margin-top: 20px;
+    padding: 8px 8px 8px 8px;
+    font-size: 16px;
+  }
+
+  .submit {
+    margin-top: 25px;
+    margin-bottom: 15px;
+    padding: 10px 10px 10px 10px;
+    font-size: 18px;
+    text-align: center;
+  }
+    
+  .submit .submit_button {
+    border-radius: 2px;
+    border-color: black;
+  }
   .listing {
 		margin:75px;
 		margin-bottom: -75px;
